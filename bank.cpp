e953521c58b680901fe796c310789dc75d111c02
kcbiradar <khajabiradar360@gmail.com>
2024-01-22 16:49:23 +0530

    methods implemented

--- bank.cpp
+++ bank.cpp
@@ -15,7 +15,7 @@ class Bank {
 
 void Bank::menu() {
     p:
-    system("cls");
+    system("clear");
     cout << "\n\n\t\t\tControl Panel";
     cout << "\n\n 1. Bank Management";
     cout << "\n 2. ATM Management";
@@ -26,20 +26,22 @@ void Bank::menu() {
     char ch;
     switch(choice) {
         case 1:
-            system("cls");
+            system("clear");
             // checking the login details
             cout << "\n\n\t\t\tLogin Account";
             cout << "\n\n E-mail : ";
             cin >> email;
             cout << "\n\n\t\tPin Code : ";
-            for(int i = 0; i < 4; i++) {
+            for(int i = 1; i <= 4; i++) {
                 ch = getchar();
+               // cin >> ch;
                 pin += ch;
                 cout << "*";
             }
             cout << "\n\n Password : ";
-            for(int i = 0; i < 4; i++) {
+            for(int i = 1; i <= 4; i++) {
                 ch = getchar();
+                //cin >> ch;
                 password += ch;
                 cout << "*";
             }
@@ -62,11 +64,81 @@ void Bank::menu() {
 }
 
 void Bank :: bank_management() {
-
+    p:
+    system("clear");
+    int choice;
+    cout << "\n\n\t\tATM Management System";
+    cout << "\n\n 1. New user";
+    cout << "\n 2. Already user";
+    cout << "\n 3. Deposit option";
+    cout << "\n 4. Withdraw option";
+    cout << "\n 5. Transfer option";
+    cout << "\n 6. Payement option";
+    cout << "\n 7. Search user record";
+    cout << "\n 8. Edit user record";
+    cout << "\n 9. Delete user record";
+    cout << "\n 10. Show all records";
+    cout << "\n 11. Payment all records";
+    cout << "\n 12. Go Back";
+    cout << "\n\n Enter your choice : ";
+    cin >> choice;
+    switch(choice) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 12:
+            menu();
+        default :
+            cout << "\n\n Invalid value...Please try again...";
+    }
+    getchar();
+    goto p;
 }
 
 void Bank :: atm_management() {
-
+    p:
+    system("clear");
+    int choice;
+    cout << "\n\n\t\tATM Management System";
+    cout << "\n\n 1. User Login & Check Balance";
+    cout << "\n 2. Withdraw Amount";
+    cout << "\n 3. Account Details";
+    cout << "\n 4. Go Back";
+    cout << "\n\n Enter your choice : ";
+    cin >> choice;
+    switch(choice) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            menu();
+        default :
+            cout << "\n\n Invalid value...Please try again...";
+    }
+    getchar();
+    goto p;
 }
 
 int main() {
